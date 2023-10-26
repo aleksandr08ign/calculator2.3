f1ade8b11fb88bc1f7c5da548ca6bca5efcd40f6
Александр <ignatenko.ap@yandex.ru>
2023-10-26 16:51:47 +0300

    calculator

---
+++ src/main/java/ru/skypro/calculator2/Calculator2Application.java
@@ -0,0 +1,14 @@
+package ru.skypro.calculator2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Calculator2Application {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(Calculator2Application.class, args);
+	}
+
+}

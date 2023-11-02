980a332093561f19d8f6dbf579af1dbb41e0b807
Александр <ignatenko.ap@yandex.ru>
2023-11-02 12:15:03 +0300

    calculator

--- src/main/java/ru/skypro/calculator/service/impl/CalculatorServiceimpl.java
+++ src/main/java/ru/skypro/calculator/service/impl/CalculatorServiceimpl.java
@@ -4,27 +4,31 @@ import org.springframework.stereotype.Service;
 import ru.skypro.calculator.service.CalculatorService;
 
 @Service
-public class CalculatorServiceimpl implements CalculatorService {
+public abstract class CalculatorServiceimpl implements CalculatorService {
 
     @Override
     public String plus(int num1, int num2) {
-        int sum = num1 + num2;
-        return plus();
+        int result = num1 + num2;
+        return String.valueOf(result);
     }
+
 
     @Override
     public String minus(int num1, int num2) {
-        return num1 + " - " + num2 + " = " + (num1 - num2);
+        int result = num1 - num2;
+        return String.valueOf(result);
     }
 
     @Override
     public String multiply(int num1, int num2) {
-        return num1 + " * " + num2 + " = " + (num1 * num2);
+        int result = num1 * num2;
+        return String.valueOf(result);
     }
 
     @Override
     public String divide(int num1, int num2) {
-        return num1 + " / " + num2 + " = " + (num1 / num2);
+        int result = num1 / num2;
+        return String.valueOf(result);
     }
 
 
